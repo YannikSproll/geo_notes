90d408b612dfb777bc805aa2e1e2abf837139208
ukgsr <ukgsr@student.kit.edu>
2023-11-30 10:56:59 +0100

    Initial commit.

---
+++ tools/css/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../../wwwroot/**/*.{js,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+}
+
